ec49c0acbf6908a91eadc8df9db855b6583efd37
xanderazuaje <charon-mortis@pm.me>
2023-12-29 03:20:05 +0100

    feat: about me section

---
+++ src/utils/copy.ts
@@ -0,0 +1,5 @@
+const copy = {
+    description: "👋 Hi, I'm Xander, a Software Developer with 2+ years of experience. Passionate about problem-solving in IT, I'm honing my skills at 42 Madrid. Eager to apply my knowledge and enthusiasm to your team!"
+}
+
+export default copy
